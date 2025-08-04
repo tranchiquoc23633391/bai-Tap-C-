9c54fa862e7deef5e7d6bce0a209d2d185532bdb
Trần Chí Quốc <23633391.quoc@student.iuh.edu.vn>
2025-08-04 14:48:44 +0700

    delete element

---
+++ funcion/ex-3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        Console.Write("Nhập phần tử cần xóa (X): ");
+        int x = int.Parse(Console.ReadLine());
+        Delete(array, x);
+    }
+
+    static void Delete(int[] a, int x)
+    {
+        int index = -1;
+
+       
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] == x)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1)
+        {
+            Console.WriteLine("Phần tử không tồn tại trong mảng.");
+            return;
+        }
+
+        // Dồn phần tử phía sau lên
+        for (int i = index; i < a.Length - 1; i++)
+        {
+            a[i] = a[i + 1];
+        }
+
+       
+        Console.WriteLine("Mảng sau khi xoá phần tử:");
+        for (int i = 0; i < a.Length - 1; i++)
+        {
+            Console.Write(a[i] + " ");
+        }
+    }
+}
