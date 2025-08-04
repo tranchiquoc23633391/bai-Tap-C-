82343c2657205ea212a06e5bf9d1bd7dbc72f40b
Trần Chí Quốc <23633391.quoc@student.iuh.edu.vn>
2025-08-04 15:10:03 +0700

    count string

---
+++ funcion/ex-4/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string a = "xin2chao";
+        int dem = countLetter(a);
+        Console.WriteLine("chuoi co " + dem + " chu");
+    }
+
+    static int countLetter(string a)
+    {
+        int count = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            if ((a[i] >= 'a' && a[i] <= 'z') || (a[i] >= 'A' && a[i] <= 'Z'))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
