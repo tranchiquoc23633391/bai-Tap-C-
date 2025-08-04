a671eedfd7c8c1030efbf77d49059ae6a5201940
Trần Chí Quốc <23633391.quoc@student.iuh.edu.vn>
2025-08-04 13:49:45 +0700

    min

---
+++ funcion/ex-2/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] array = { 6, 5, 4, 3, 2, 1 };
+        int index = minArray(array);
+        Console.WriteLine("gia tri nho nhat la :" + index);
+    }
+
+    static int minArray(int[] a)  // thêm từ khóa static ở đây
+    {
+        int min = a[0];
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] < min)
+            {
+                min = a[i];
+            }
+        }
+        return min;
+    }
+}
