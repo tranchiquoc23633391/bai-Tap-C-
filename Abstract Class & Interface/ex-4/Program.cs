c1a77f4d8ffce0a009a719ed1b2721188cbb0ade
Trần Chí Quốc <23633391.quoc@student.iuh.edu.vn>
2025-08-14 13:41:06 +0700

    Resizeable
    Resizeable

---
+++ Abstract Class & Interface/ex-4/Program.cs
@@ -0,0 +1,273 @@
+﻿using System;
+
+
+public interface IResize
+{
+    void Resize(double percent);
+}
+
+
+namespace Shape
+{
+    public class Circle : Shape, IResize
+    {
+        private double radius = 1.0;
+
+        public Circle()
+        {
+        }
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public Circle(double radius, String color, bool filled) : base(color, filled)
+        {
+            this.radius = radius;
+        }
+
+        public double getRadius()
+        {
+            return radius;
+        }
+
+        public void setRadius(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double getArea()
+        {
+            return radius * radius * Math.PI;
+        }
+
+        public double getPerimeter()
+        {
+            return 2 * radius * Math.PI;
+        }
+
+        public override string ToString()
+        {
+            return "A Circle with radius="
+                    + getRadius()
+                    + ", which is a subclass of "
+                    + base.ToString();
+        }
+
+        public void Resize(double percent)
+        {
+            radius += radius * percent / 100;
+        }
+    }
+}
+
+
+namespace Shape
+{
+    public class Rectangle : Shape, IResize
+    {
+        private double width = 1.0;
+        private double length = 1.0;
+
+        public Rectangle()
+        {
+        }
+
+        public Rectangle(double width, double length)
+        {
+            this.width = width;
+            this.length = length;
+        }
+
+        public Rectangle(double width, double length, string color, bool filled) : base(color, filled)
+        {
+            this.width = width;
+            this.length = length;
+        }
+
+        public double getWidth()
+        {
+            return width;
+        }
+
+        public virtual void setWidth(double width)
+        {
+            this.width = width;
+        }
+
+        public double getLength()
+        {
+            return length;
+        }
+
+        public virtual void setLength(double length)
+        {
+            this.length = length;
+        }
+
+        public double getArea()
+        {
+            return width * this.length;
+        }
+
+        public double getPerimeter()
+        {
+            return 2 * (width + this.length);
+        }
+
+        public override string ToString()
+        {
+            return "A Rectangle with width="
+                    + getWidth()
+                    + " and length="
+                    + getLength()
+                    + ", which is a subclass of "
+                    + base.ToString();
+        }
+
+        public void Resize(double percent)
+        {
+            length += length * percent / 100;
+            width += width * percent / 100;
+        }
+    }
+}
+
+namespace Shape
+{
+    public class Shape
+    {
+        private string color = "green";
+        private bool filled = true;
+
+        public Shape()
+        {
+        }
+
+        public Shape(string color, bool filled)
+        {
+            this.color = color;
+            this.filled = filled;
+        }
+
+        public string getColor()
+        {
+            return color;
+        }
+
+        public void setColor(string color)
+        {
+            this.color = color;
+        }
+
+        public bool isFilled()
+        {
+            return filled;
+        }
+
+        public void setFilled(bool filled)
+        {
+            this.filled = filled;
+        }
+
+        public override string ToString()
+        {
+            return "A Shape with color of "
+                    + getColor()
+                    + " and "
+                    + (isFilled() ? "filled" : "not filled");
+        }
+    }
+}
+
+namespace Shape
+{
+    public class Square : Rectangle, IResize
+    {
+        public Square()
+        {
+        }
+
+        public Square(double side) : base(side, side)
+        {
+        }
+
+        public Square(double side, string color, bool filled) : base(side, side, color, filled)
+        {
+        }
+
+        public double getSide()
+        {
+            return getWidth();
+        }
+
+        public void setSide(double side)
+        {
+            base.setWidth(side);
+            base.setLength(side);
+        }
+        public override void setWidth(double width)
+        {
+            setSide(width);
+        }
+
+        public override void setLength(double length)
+        {
+            setSide(length);
+        }
+
+
+        public void Resize(double percent)
+        {
+            double side = getWidth(); 
+            side += side * percent / 100;
+            base.setWidth(side);
+            base.setLength(side);
+        }
+
+        public override string ToString()
+        {
+            return "A Square with side="
+                    + getSide()
+                    + ", which is a subclass of "
+                    + base.ToString();
+        }
+    }
+}
+
+
+
+namespace Shape
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IResize[] shapes = new IResize[]
+            {
+                new Circle(5),
+                new Rectangle(4, 6),
+                new Square(4)
+            };
+
+            Random rand = new Random();
+
+
+            foreach (var shape in shapes)
+            {
+
+                Console.WriteLine($"Before Resize: {shape}");
+
+              
+                double percent = rand.Next(1, 101);
+                Console.WriteLine($"Resize by {percent}%");
+
+              
+                shape.Resize(percent);
+
+                Console.WriteLine($"After Resize: {shape}\n");
+            }
+        }
+    }
+}
