232808c3a2d14246348f5e480702f5fbf75f6a37
Trần Chí Quốc <23633391.quoc@student.iuh.edu.vn>
2025-08-14 13:59:09 +0700

    IColorable
    IColorable

---
+++ Abstract Class & Interface/ex-5/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+
+
+public interface IColorable
+{
+    string howToColor();
+}
+
+
+
+namespace Shape
+{
+    public abstract class Shape
+    {
+        private string color = "green";
+        private bool filled = true;
+
+        public Shape()
+        {
+        }
+
+        public Shape(string color, bool filled)
+        {
+            this.color = color;
+            this.filled = filled;
+        }
+
+        public string getColor()
+        {
+            return color;
+        }
+
+        public void setColor(string color)
+        {
+            this.color = color;
+        }
+
+        public bool isFilled()
+        {
+            return filled;
+        }
+
+        public void setFilled(bool filled)
+        {
+            this.filled = filled;
+        }
+
+        public abstract double getArea();
+        public override string ToString()
+        {
+            return "A Shape with color of "
+                    + getColor()
+                    + " and "
+                    + (isFilled() ? "filled" : "not filled");
+        }
+    }
+}
+
+
+namespace Shape
+{
+    public class Rectangle : Shape
+    {
+        private double width = 1.0;
+        private double length = 1.0;
+
+        public Rectangle()
+        {
+        }
+
+        public Rectangle(double width, double length)
+        {
+            this.width = width;
+            this.length = length;
+        }
+
+        public Rectangle(double width, double length, string color, bool filled) : base(color, filled)
+        {
+            this.width = width;
+            this.length = length;
+        }
+
+        public double getWidth()
+        {
+            return width;
+        }
+
+        public virtual void setWidth(double width)
+        {
+            this.width = width;
+        }
+
+        public double getLength()
+        {
+            return length;
+        }
+
+        public virtual void setLength(double length)
+        {
+            this.length = length;
+        }
+
+        public double getPerimeter()
+        {
+            return 2 * (width + this.length);
+        }
+
+        public override string ToString()
+        {
+            return "A Rectangle with width="
+                    + getWidth()
+                    + " and length="
+                    + getLength()
+                    + ", which is a subclass of "
+                    + base.ToString();
+        }
+
+        public override double getArea()
+        {
+            return width * this.length;
+        }
+    }
+}
+
+namespace Shape
+{
+    public class Square : Rectangle, IColorable
+    {
+        public Square()
+        {
+        }
+
+        public Square(double side) : base(side, side)
+        {
+        }
+
+        public Square(double side, string color, bool filled) : base(side, side, color, filled)
+        {
+        }
+
+        public double getSide()
+        {
+            return getWidth();
+        }
+
+        public void setSide(double side)
+        {
+            base.setWidth(side);
+            base.setLength(side);
+        }
+        public override void setWidth(double width)
+        {
+            setSide(width);
+        }
+
+        public override void setLength(double length)
+        {
+            setSide(length);
+        }
+
+        public override string ToString()
+        {
+            return "A Square with side="
+                    + getSide()
+                    + ", which is a subclass of "
+                    + base.ToString();
+        }
+
+        public string howToColor()
+        {
+            return "Color all four sides";
+        }
+    }
+}
+
+
+namespace Shape
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Shape[] shapes = new Shape[]
+            {
+                new Rectangle(4, 6),
+                new Square(4),
+                new Rectangle(2, 5)
+            };
+
+
+            foreach (var shape in shapes)
+            {
+
+                Console.WriteLine($"Area: {shape.getArea()}");
+
+
+                if (shape is IColorable colorableShape)
+                {
+                    Console.WriteLine(colorableShape.howToColor());
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
