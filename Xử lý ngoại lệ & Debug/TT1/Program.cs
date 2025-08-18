f7cc77da672aab43ea1e2a72b6cf47cfb1e508b5
Trần Chí Quốc <23633391.quoc@student.iuh.edu.vn>
2025-08-18 07:35:10 +0700

    IndexOutOfRangeException
    IndexOutOfRangeException

---
+++ Xử lý ngoại lệ & Debug/TT1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Buffers;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            // int a = Convert.ToInt32(Console.ReadLine());
+            // int b = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine($"Thuong La{a / b}");
+            int x = int.Parse("abc");
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine($"ban da nhap so 0{e.Message}");
+
+        }
+        ;
+        try
+        {
+            int[] arr = new int[12];
+            Console.WriteLine(arr[13]);
+        }
+        catch (IndexOutOfRangeException e)
+        {
+            Console.WriteLine($"ngoai khoang{e.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("ket thuc");
+        }
+
+    }
+}
---
+++ Xử lý ngoại lệ & Debug/ex-1/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<char> characters = new List<char>();
+        characters.InsertRange(0, new char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+
+        try
+        {
+            List<char> innerCharacters = new List<char>();
+            innerCharacters.InsertRange(0, new char[] { 'g', 'h', 'i', 'j', 'k', 'l' });
+            char s = innerCharacters[5];
+
+        }
+        catch (IndexOutOfRangeException e)
+        {
+            System.Console.WriteLine(e);
+        }
+    }
+}
