6361dcb649c597cfa535cfc493d2adb4ef76bc9a
Trần Chí Quốc <23633391.quoc@student.iuh.edu.vn>
2025-08-19 18:00:34 +0700

    Hiển thị lời chào

---
+++ Biến, kiểu dữ liệu và toán tử/Hiển thị lời chào/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name: ");
+
+        string yourName = Console.ReadLine();
+
+        Console.WriteLine("Hello: " + yourName);
+    }
+}
