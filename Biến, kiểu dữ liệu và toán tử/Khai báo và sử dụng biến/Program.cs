083fb6cc54d8269dec7f79cc2a1d2bf3a815c3e5
Trần Chí Quốc <23633391.quoc@student.iuh.edu.vn>
2025-08-19 17:57:42 +0700

    Khai báo và sử dụng biến

---
+++ Biến, kiểu dữ liệu và toán tử/Khai báo và sử dụng biến/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        bool b = true;
+        char c = 'a';
+        string s = "CodeGym";
+
+        Console.WriteLine("Giá trị của biến i là " + i);
+        Console.WriteLine("Giá trị của biến f là " + f);
+        Console.WriteLine("Giá trị của biến d là " + d);
+        Console.WriteLine("Giá trị của biến b là " + b);
+        Console.WriteLine("Giá trị của biến c là " + c);
+        Console.WriteLine("Giá trị của biến s là " + s);
+    }
+}
