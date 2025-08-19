65066beca35dfd24508e559d5386df68e3dbd1ce
Trần Chí Quốc <23633391.quoc@student.iuh.edu.vn>
2025-08-19 18:02:23 +0700

    Ứng dụng chuyển đổi tiền tệ

---
+++ Biến, kiểu dữ liệu và toán tử/Ứng dụng chuyển đổi tiền tệ/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Bước 2: Khai báo biến tỉ giá
+        double rate = 23000;
+
+        // Bước 3: Nhập giá trị USD từ người dùng
+        Console.Write("Enter the amount in USD: ");
+        double usd = double.Parse(Console.ReadLine());
+
+        // Bước 4: Tính và hiển thị giá trị VND
+        double vnd = usd * rate;
+        Console.WriteLine("Equivalent amount in VND: " + vnd);
+    }
+}
