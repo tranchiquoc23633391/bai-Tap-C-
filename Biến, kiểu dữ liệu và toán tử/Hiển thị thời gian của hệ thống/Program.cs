c7fa53da38d68589ed91d1261dcea96e3ab4d457
Trần Chí Quốc <23633391.quoc@student.iuh.edu.vn>
2025-08-19 17:54:06 +0700

    Hiển thị thời gian của hệ thống

---
+++ Biến, kiểu dữ liệu và toán tử/Hiển thị thời gian của hệ thống/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime localDate = DateTime.Now;
+        Console.WriteLine("Datetime Now is: " + localDate);
+    }
+}
---
+++ File & Serialization/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // string sourceFilePath = "E:\\Source.txt";
+        // string destinationFilePath = "E:\\Destination.txt";
+        // File.Copy(sourceFilePath, destinationFilePath, true);
+
+        // Console.WriteLine($"File copied from {sourceFilePath} to {destinationFilePath}. File exists: {File.Exists(destinationFilePath)}");
+
+
+        // FileInfo fileInfo = new FileInfo(destinationFilePath);
+        // Console.WriteLine($"File size: {fileInfo.Length} bytes");
+
+
+        string filePath = "E:\\FileNew";
+        // Directory.CreateDirectory(filePath);
+        DirectoryInfo directoryInfo = new DirectoryInfo(filePath);
+        directoryInfo.Create();
+
+
+    }
+}
